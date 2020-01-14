e38f8f67135f649de0587cfce2fd7910b7633b16
Lucas López Vélez <dev@luckslovez.me>
2020-01-14 11:09:13 +0400

    develop
    Add `ResourceChildrenInjector`

---
+++ sling-models-extensions-injectors/src/main/java/me/luckslovez/sling/models/extensions/injectors/ResourceChildrenInjector.java
@@ -0,0 +1,117 @@
+package me.luckslovez.sling.models.extensions.injectors;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.spi.DisposalCallbackRegistry;
+import org.apache.sling.models.spi.Injector;
+import org.apache.sling.models.spi.injectorspecific.AbstractInjectAnnotationProcessor2;
+import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessor2;
+import org.apache.sling.models.spi.injectorspecific.InjectAnnotationProcessorFactory2;
+import org.jetbrains.annotations.NotNull;
+import org.osgi.service.component.annotations.Component;
+
+import me.luckslovez.sling.models.extensions.injectors.annotations.ResourceChildren;
+
+@Component
+public class ResourceChildrenInjector implements Injector, InjectAnnotationProcessorFactory2 {
+
+    @Override
+    public String getName() {
+        return "resource-children";
+    }
+
+    @Override
+    public Object getValue(@NotNull Object adaptable, @NotNull String fieldName, @NotNull Type type,
+            @NotNull AnnotatedElement annotatedElement, @NotNull DisposalCallbackRegistry disposalCallbackRegistry) {
+
+        final ResourceChildren resourceChildrenAnnotation = annotatedElement.getAnnotation(ResourceChildren.class);
+
+        if (resourceChildrenAnnotation != null) {
+            final Resource resource = adaptable instanceof SlingHttpServletRequest
+                    ? ((SlingHttpServletRequest) adaptable).getResource()
+                    : (Resource) adaptable;
+
+            final String name = resourceChildrenAnnotation.name();
+
+            final Optional<Class> adapter = Optional.of(annotatedElement)
+                    .map(Field.class::cast)
+                    .map(Field::getGenericType)
+                    .flatMap(this::resolveClass)
+                    .filter(clazz -> clazz != Resource.class);
+
+            Stream<Resource> result;
+
+            if (StringUtils.isBlank(name)) {
+                result = StreamSupport.stream(resource.getChildren().spliterator(), false);
+            } else if (resource.getChild(name) != null) {
+                result = StreamSupport.stream(resource.getChild(name).getChildren().spliterator(), false);
+            } else {
+                result = Stream.empty();
+            }
+
+            return adapter.isPresent()
+                    ? result
+                    .map(child -> child.adaptTo(adapter.get()))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList())
+                    : result.collect(Collectors.toList());
+        }
+
+        return null;
+    }
+
+    private Optional<Class> resolveClass(Type type) {
+        return Optional.of(type)
+                .filter(ParameterizedType.class::isInstance)
+                .map(ParameterizedType.class::cast)
+                .map(ParameterizedType::getActualTypeArguments)
+                .stream()
+                .flatMap(Arrays::stream)
+                .findAny()
+                .map(Class.class::cast);
+    }
+
+    @Override
+    public InjectAnnotationProcessor2 createAnnotationProcessor(@NotNull Object adaptable, @NotNull AnnotatedElement annotatedElement) {
+
+        return Optional.of(annotatedElement)
+                .map(element -> element.getAnnotation(ResourceChildren.class))
+                .map(ChildResourceChildrenProcessor::new)
+                .orElse(null);
+
+    }
+
+    private static class ChildResourceChildrenProcessor extends AbstractInjectAnnotationProcessor2 {
+
+        private final ResourceChildren annotation;
+
+        ChildResourceChildrenProcessor(@NotNull ResourceChildren annotation) {
+            this.annotation = annotation;
+        }
+
+        @Override
+        public String getName() {
+            return annotation.name();
+        }
+
+        @Override
+        public InjectionStrategy getInjectionStrategy() {
+            return annotation.injectionStrategy();
+        }
+
+    }
+
+}
---
+++ sling-models-extensions-injectors/src/test/java/me/luckslovez/sling/models/extensions/injectors/ResourceChildrenInjectorTest.java
@@ -0,0 +1,92 @@
+package me.luckslovez.sling.models.extensions.injectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.factory.ModelFactory;
+import org.apache.sling.testing.mock.sling.junit5.SlingContext;
+import org.apache.sling.testing.mock.sling.junit5.SlingContextExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.google.common.collect.ImmutableMap;
+
+import me.luckslovez.sling.models.extensions.injectors.annotations.ResourceChildren;
+
+@ExtendWith(SlingContextExtension.class)
+class ResourceChildrenInjectorTest {
+
+    private static final String CHILD = "child";
+    private static final String PROP = "prop";
+
+    private final SlingContext context = new SlingContext();
+
+    private Resource testResource;
+
+    @BeforeEach
+    public void setUp() {
+        context.addModelsForPackage("me.luckslovez.sling.models.extensions.injectors");
+        context.registerInjectActivateService(new ResourceChildrenInjector());
+        testResource = context.create().resource("/resource");
+    }
+
+    @Test
+    public void testResourceList() {
+        context.create().resource("/resource/child", ImmutableMap.of(PROP, PROP));
+        ResourceListModel testModel = context.getService(ModelFactory.class).createModel(testResource, ResourceListModel.class);
+
+        assertNotNull(testModel);
+        assertEquals(CHILD, testModel.children.get(0).getName());
+        assertEquals(PROP, testModel.children.get(0).getValueMap().get(PROP, String.class));
+    }
+
+    @Test
+    public void testSimpleModelList() {
+        context.create().resource("/resource/children/" + CHILD, ImmutableMap.of(PROP, PROP));
+        SimpleModelListModel testModel = context.getService(ModelFactory.class).createModel(testResource, SimpleModelListModel.class);
+
+        assertNotNull(testModel);
+        assertEquals(PROP, testModel.children.get(0).prop);
+    }
+
+    @Test
+    public void testChildResourceSimpleModelListModel() {
+        context.create().resource("/resource/relative/path/" + CHILD, ImmutableMap.of(PROP, PROP));
+        ChildResourceSimpleModelListModel testModel = context.getService(ModelFactory.class)
+                .createModel(testResource, ChildResourceSimpleModelListModel.class);
+
+        assertNotNull(testModel);
+        assertEquals(PROP, testModel.children.get(0).prop);
+    }
+
+    @Model(adaptables = Resource.class)
+    public static final class ResourceListModel {
+        @ResourceChildren(name = "")
+        public List<Resource> children;
+    }
+
+    @Model(adaptables = Resource.class)
+    public static final class SimpleModelListModel {
+        @ResourceChildren(name = "children")
+        public List<SinglePropModel> children;
+    }
+
+    @Model(adaptables = Resource.class)
+    public static final class ChildResourceSimpleModelListModel {
+        @ResourceChildren(name = "relative/path")
+        public List<SinglePropModel> children;
+    }
+
+    @Model(adaptables = Resource.class)
+    public static final class SinglePropModel {
+        @ValueMapValue
+        public String prop;
+    }
+
+}
