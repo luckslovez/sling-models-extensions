28581676eeb9c4a2ec752fa974263ee4b6a19f34
Lucas López Vélez <dev@luckslovez.me>
2020-01-15 19:13:40 +0400

    Enhance javadoc

--- sling-models-extensions-injectors/src/main/java/me/luckslovez/sling/models/extensions/services/MapFactory.java
+++ sling-models-extensions-injectors/src/main/java/me/luckslovez/sling/models/extensions/services/MapFactory.java
@@ -9,9 +9,9 @@ public interface MapFactory {
     /**
      * Given a {@link String[]} of <i>key{separator}value</i>, splits each by <i>{separator}</i> and builds a map.
      *
-     * @param keyValues
-     * @param separator
-     * @return
+     * @param keyValues {@link String[]} where each element contains a key, a separator and a value
+     * @param separator separator used to split key an value
+     * @return respective Map
      */
     @NotNull
     Map<String, Object> keyValuesToMap(@NotNull String[] keyValues, @NotNull String separator);
